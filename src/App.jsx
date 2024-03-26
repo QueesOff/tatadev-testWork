22b22784914712a40a72d0cb31cbc1f856b23c02
DaimenGGer <daimenkg@gmail.com>
2024-03-26 16:36:00 +0600

    v3

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   useEffect(() => {
     const timer = setTimeout(() => {
       setContentVisible(true);
-    }, 20); // Имитируем задержку в 2 секунды перед появлением контента
+    }, 2000); // Имитируем задержку в 2 секунды перед появлением контента
 
     return () => clearTimeout(timer);
   }, []);
@@ -34,7 +34,42 @@ function App() {
   ];
 
   return (
-    <div className="w-full h-screen relative flex items-center justify-center">
+    <div className="w-full h-screen relative flex flex-col gap-[200px] items-center justify-center p-10 mt-10">
+      {imageLoaded && (
+        <header
+          className={`flex justify-between p-5 w-[1200px] mt-[-200px] rounded-[20px] bg-[#67BA75] content ${
+            contentVisible ? "contentVisible" : ""
+          }`}
+        >
+          <div className="flex gap-3">
+            {iconsLeft.map((icon, index) => (
+              <img
+                key={index}
+                src={icon.src}
+                alt={icon.alt}
+                className="w-[32px] h-[32px] rounded-lg bg-white p-2"
+              />
+            ))}
+          </div>
+          <ul className="flex gap-5">
+            {nav.map((content, index) => (
+              <li key={index} className="text-white text-[18px]">
+                {content.title}
+              </li>
+            ))}
+          </ul>
+          <div className="flex gap-3">
+            {iconsRight.map((icon, index) => (
+              <img
+                key={index}
+                src={icon.src}
+                alt={icon.alt}
+                className="w-[32px] h-[32px] rounded-lg bg-white p-2"
+              />
+            ))}
+          </div>
+        </header>
+      )}
       <div
         className={`absolute image-container ${
           contentVisible ? "image-container-small" : ""
@@ -49,63 +84,36 @@ function App() {
       </div>
       {imageLoaded && ( // Показываем контент только после загрузки изображения
         <div
-          className={`flex flex-col content ${
+          className={` flex flex-col content ${
             contentVisible ? "contentVisible" : ""
           }`}
         >
-          <header className="flex justify-between p-5 w-[1200px] rounded-[20px] bg-[#67BA75]">
-            <div className="flex gap-3">
-              {iconsLeft.map((icon, index) => (
-                <img
-                  key={index}
-                  src={icon.src}
-                  alt={icon.alt}
-                  className="w-[32px] h-[32px] rounded-lg bg-white p-2"
-                />
-              ))}
-            </div>
-            <ul className="flex gap-5">
-              {nav.map((content, index) => (
-                <li key={index} className="text-white text-[18px]">
-                  {content.title}
-                </li>
-              ))}
-            </ul>
-            <div className="flex gap-3">
-              {iconsRight.map((icon, index) => (
-                <img
-                  key={index}
-                  src={icon.src}
-                  alt={icon.alt}
-                  className="w-[32px] h-[32px] rounded-lg bg-white p-2"
-                />
-              ))}
-            </div>
-          </header>
-          <h1 className="text-[#003458] text-[30px] font-bold text-center">
-            Чистый мир – Ваш путь <br /> к идеальной чистоте и комфорту!
-          </h1>
-          <form
-            action=""
-            className="flex w-[337px] gap-5 flex-col items-center text-[#003458] "
-          >
-            <input
-              type="text"
-              placeholder="Ваше имя"
-              className="w-full border-b-[1px] border-[#003458] placeholder-[#175783]"
-            />
-            <input
-              type="text"
-              placeholder="Ваш номер телефона или WhatsApp"
-              className="w-full border-b-[1px] border-[#003458] placeholder-[#175783]"
-            />
-            <button
-              type="button"
-              className="w-full bg-[#67BA75] rounded-[20px] text-center p-2"
+          <div className="flex flex-col items-center justify-center">
+            <h1 className="text-[#003458] mb-[30px] text-[30px] font-bold text-center">
+              Чистый мир – Ваш путь <br /> к идеальной чистоте и комфорту!
+            </h1>
+            <form
+              action=""
+              className="flex w-[337px] gap-6 flex-col items-center text-[#003458]"
             >
-              Заказать уборку
-            </button>
-          </form>
+              <input
+                type="text"
+                placeholder="Ваше имя"
+                className="w-full border-b-[1px] border-[#003458] placeholder-[#175783] pl-2 pb-1"
+              />
+              <input
+                type="text"
+                placeholder="Ваш номер телефона или WhatsApp"
+                className="w-full border-b-[1px] border-[#003458] placeholder-[#175783] pl-2 pb-1"
+              />
+              <button
+                type="button"
+                className="w-full bg-[#67BA75] rounded-[20px] text-center p-4 text-white mt-3"
+              >
+                Заказать уборку
+              </button>
+            </form>
+          </div>
         </div>
       )}
     </div>
